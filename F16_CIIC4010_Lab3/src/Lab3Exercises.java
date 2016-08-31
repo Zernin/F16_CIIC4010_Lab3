a59b3d27c31857bcfaccab71e35cc150cc9ae760
4015a5 <4015a5@tamarindo.ece.uprm.edu>
2016-08-31 08:28:39 -0400

    Changed the title, location and size of the JFrame

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Jonathan");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900,400);
+		myFrame.setLocation(950,400);
 		myFrame.setSize(200,200);
 		myFrame.setVisible(true);
 	}
